305611c91ba96d36fcf7647059e6c06a8f1ce0f1
DaniAkash <s.daniakash@gmail.com>
2019-07-19 19:04:53 +0530

    add communication with the webview

--- index.js
+++ index.js
@@ -1,10 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
+import { ViewPropTypes } from "react-native";
 import WebView from "react-native-webview";
+import PropTypes from "prop-types";
 
 const draftJsHtml = require("./draftjs-html-source/index.html");
 
-const RNDraftView = () => {
-  return <WebView style={{ flex: 1 }} source={draftJsHtml} />;
-};
+class RNDraftView extends Component {
+  static propTypes = {
+    style: ViewPropTypes.style,
+    onStyleChanged: PropTypes.func,
+    onBlockTypeChanged: PropTypes.func
+  };
+
+  _webViewRef = React.createRef();
+
+  setBlockType = blockType => {
+    this._webViewRef.current &&
+      this._webViewRef.current.injectJavaScript(`
+        window.toggleBlockType("${blockType}");
+        true;
+      `);
+  };
+
+  setStyle = style => {
+    this._webViewRef.current &&
+      this._webViewRef.current.injectJavaScript(`
+        window.toggleInlineStyle("${style}");
+        true;
+      `);
+  };
+
+  onMessage = event => {
+    const {
+      onStyleChanged = () => null,
+      onBlockTypeChanged = () => null
+    } = this.props;
+    const { data } = event.nativeEvent;
+    const { blockType, styles } = JSON.parse(data);
+    onBlockTypeChanged(blockType);
+    onStyleChanged(styles ? styles.split(",") : []);
+  };
+
+  render() {
+    const { style = { flex: 1 } } = this.props;
+    return (
+      <WebView
+        ref={this._webViewRef}
+        style={style}
+        source={draftJsHtml}
+        onMessage={this.onMessage}
+      />
+    );
+  }
+}
 
 export default RNDraftView;
