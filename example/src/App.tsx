f4ffe23fa3d45a5a695b748aad8ad5a850015a43
DaniAkash <s.daniakash@gmail.com>
2020-10-07 19:59:30 +0530

    feat: update example project

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -18,13 +18,25 @@ import {
 import KeyboardSpacer from 'react-native-keyboard-spacer';
 import RNDraftView from 'react-native-draftjs-editor';
 
-const ControlButton = ({ text, action, isActive }) => {
+export interface ControlButtonProps {
+  text: string;
+  action: () => unknown;
+  isActive: boolean;
+}
+
+export interface EditorToolBar {
+  activeStyles: string[];
+  blockType: string;
+  toggleStyle: (style: string) => unknown;
+  toggleBlockType: (block: string) => unknown;
+}
+
+const ControlButton = ({ text, action, isActive }: ControlButtonProps) => {
+  const backgroundColor = { backgroundColor: 'gold' };
+
   return (
     <TouchableOpacity
-      style={[
-        styles.controlButtonContainer,
-        isActive ? { backgroundColor: 'gold' } : {},
-      ]}
+      style={[styles.controlButtonContainer, isActive ? backgroundColor : {}]}
       onPress={action}
     >
       <Text>{text}</Text>
@@ -37,7 +49,7 @@ const EditorToolBar = ({
   blockType,
   toggleStyle,
   toggleBlockType,
-}) => {
+}: EditorToolBar) => {
   return (
     <View style={styles.toolbarContainer}>
       <ControlButton
@@ -81,8 +93,8 @@ const styleMap = {
 };
 
 const App = () => {
-  const _draftRef = React.createRef();
-  const [activeStyles, setActiveStyles] = useState([]);
+  const _draftRef = React.createRef<RNDraftView>();
+  const [activeStyles, setActiveStyles] = useState<string[]>([]);
   const [blockType, setActiveBlockType] = useState('unstyled');
   const [editorState, setEditorState] = useState('');
 
@@ -90,15 +102,19 @@ const App = () => {
     '<h1>A Full fledged Text Editor</h1><p>This editor is built with Draft.js. Hence should be suitable for most projects. However, Draft.js Isn’t fully compatible with mobile yet. So you might face some issues.</p><p><br></p><p>This is a simple implementation</p><ul>  <li>It contains <strong>Text formatting </strong>and <em>Some blocks formatting</em></li>  <li>Each for it’s own purpose</li></ul><p>You can also do</p><ol>  <li>Custom style map</li>  <li>Own css styles</li>  <li>Custom block styling</li></ol><p>You are welcome to try it!</p>';
 
   const editorLoaded = () => {
-    _draftRef.current && _draftRef.current.focus();
+    _draftRef.current?.focus();
   };
 
-  const toggleStyle = (style) => {
-    _draftRef.current && _draftRef.current.setStyle(style);
+  const toggleStyle = (style: string) => {
+    _draftRef.current?.setStyle(style);
   };
 
-  const toggleBlockType = (blockType) => {
-    _draftRef.current && _draftRef.current.setBlockType(blockType);
+  const toggleBlockType = (setBlockType: string) => {
+    _draftRef.current?.setBlockType(setBlockType);
+  };
+
+  const updateActiveStyle = (styles: string[]) => {
+    setActiveStyles(styles);
   };
 
   useEffect(() => {
@@ -108,6 +124,7 @@ const App = () => {
      */
     setEditorState(_draftRef.current ? _draftRef.current.getEditorState() : '');
   }, [_draftRef]);
+
   console.log(editorState);
 
   return (
@@ -115,10 +132,9 @@ const App = () => {
       <RNDraftView
         defaultValue={defaultValue}
         onEditorReady={editorLoaded}
-        style={{ flex: 1 }}
         placeholder={'Add text here...'}
         ref={_draftRef}
-        onStyleChanged={setActiveStyles}
+        onStyleChanged={updateActiveStyle}
         onBlockTypeChanged={setActiveBlockType}
         styleMap={styleMap}
       />
